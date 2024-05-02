a3cf17c0f3060c5573a7656da099a56bf02094ab
KaylaPosthumusOW <231096@virtualwindow.co.za>
2024-05-02 16:16:16 +0200

    property list array

---
+++ groupactivity/src/propertyList.js
@@ -0,0 +1,68 @@
+const properties = [
+    {
+      id: 1,
+      title: 'Elevate Modern Home',
+      price: '$250,000',
+      description: 'Spacious and modern apartment located in the heart of the city.',
+      location: 'Pretoria - Moreleta Park',
+      imageUrl: '../assets/house 1.jpg',
+    },
+    {
+      id: 2,
+      title: 'Maple Grove Manor',
+      price: '$400,000',
+      description: 'Charming cottage nestled among towering trees, offering a peaceful retreat from the hustle and bustle.',
+      location: 'Pretoria - Elardus Park',
+      imageUrl: '../assets/house 2.jpg',
+    },
+    {
+        id: 3,
+        title: 'Sunset Terrace Residence',
+        price: '$350,000',
+        description: 'Enjoy picturesque views from this delightful residence, offering a peaceful ambiance and modern comforts.',
+        location: 'Pretoria - Lynnwood',
+        imageUrl: '../assets/house 3.jpg',
+      },
+      {
+        id: 4,
+        title: 'Zen Oasis House',
+        price: '$280,000',
+        description: 'Traditional elegance meets modern convenience in this inviting home, complete with a cozy fireplace and timeless appeal.',
+        location: 'Pretoria - Moreleta Park',
+        imageUrl: '../assets/house 4.jpg',
+      },
+      {
+        id: 5,
+        title: 'Classic Hearth Home',
+        price: '$430,000',
+        description: 'A secluded hideaway with rustic charm, surrounded by nature and offering a sense of tranquility',
+        location: 'Pretoria - Groenkloof',
+        imageUrl: '../assets/house 5.jpg',
+      },
+      {
+        id: 6,
+        title: 'Tranquil Suburban House',
+        price: '$290,000',
+        description: 'Spacious layout with ample room for family activities and gatherings, creating a warm and inviting atmosphere.',
+        location: 'Pretoria - Elardus Park',
+        imageUrl: '../assets/house 6.jpg',
+      },
+      {
+        id: 7,
+        title: 'Harmony House',
+        price: '$320,000',
+        description: 'A well-maintained home featuring harmonious interiors and spacious living areas ideal for family gatherings.',
+        location: 'Pretoria - Waterkloof',
+        imageUrl: '../assets/house 7.jpg',
+      },
+      {
+        id: 8,
+        title: 'Secluded Haven House',
+        price: '$380,000',
+        description: 'A serene escape with lush greenery and a private garden, perfect for relaxation and unwinding.',
+        location: 'Pretoria - Waterkloof',
+        imageUrl: '../assets/house 8.jpg',
+      },
+  ];
+  
+  export default properties;
