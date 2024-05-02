3772702a3c5d0174f39aec261dcceb09767ad8ad
KaylaPosthumusOW <231096@virtualwindow.co.za>
2024-05-02 16:30:14 +0200

    Property List Update

--- groupactivity/src/propertyList.js
+++ groupactivity/src/propertyList.js
@@ -6,6 +6,9 @@ const properties = [
       description: 'Spacious and modern apartment located in the heart of the city.',
       location: 'Pretoria - Moreleta Park',
       imageUrl: '../assets/house 1.jpg',
+      bedrooms: '4',
+      bathrooms: '2',
+      amenities: 'Braai Area, Swimming Pool, Parking Garage'
     },
     {
       id: 2,
@@ -14,6 +17,9 @@ const properties = [
       description: 'Charming cottage nestled among towering trees, offering a peaceful retreat from the hustle and bustle.',
       location: 'Pretoria - Elardus Park',
       imageUrl: '../assets/house 2.jpg',
+      bedrooms: '3',
+      bathrooms: '3',
+      amenities: 'Braai Area, Lapa, Parking Garage'
     },
     {
         id: 3,
@@ -22,6 +28,9 @@ const properties = [
         description: 'Enjoy picturesque views from this delightful residence, offering a peaceful ambiance and modern comforts.',
         location: 'Pretoria - Lynnwood',
         imageUrl: '../assets/house 3.jpg',
+        bedrooms: '6',
+        bathrooms: '4',
+        amenities: 'Swimming Pool, Parking Garage'
       },
       {
         id: 4,
@@ -30,6 +39,9 @@ const properties = [
         description: 'Traditional elegance meets modern convenience in this inviting home, complete with a cozy fireplace and timeless appeal.',
         location: 'Pretoria - Moreleta Park',
         imageUrl: '../assets/house 4.jpg',
+        bedrooms: '3',
+        bathrooms: '2',
+        amenities: 'Parking Garage'
       },
       {
         id: 5,
@@ -38,6 +50,9 @@ const properties = [
         description: 'A secluded hideaway with rustic charm, surrounded by nature and offering a sense of tranquility',
         location: 'Pretoria - Groenkloof',
         imageUrl: '../assets/house 5.jpg',
+        bedrooms: '3',
+        bathrooms: '2',
+        amenities: 'Braai Area,Parking Garage'
       },
       {
         id: 6,
@@ -46,6 +61,9 @@ const properties = [
         description: 'Spacious layout with ample room for family activities and gatherings, creating a warm and inviting atmosphere.',
         location: 'Pretoria - Elardus Park',
         imageUrl: '../assets/house 6.jpg',
+        bedrooms: '4',
+        bathrooms: '3',
+        amenities: 'Swimming Pool, Parking Garage'
       },
       {
         id: 7,
@@ -54,6 +72,9 @@ const properties = [
         description: 'A well-maintained home featuring harmonious interiors and spacious living areas ideal for family gatherings.',
         location: 'Pretoria - Waterkloof',
         imageUrl: '../assets/house 7.jpg',
+        bedrooms: '5',
+        bathrooms: '3',
+        amenities: 'Parking Garage'
       },
       {
         id: 8,
@@ -62,6 +83,9 @@ const properties = [
         description: 'A serene escape with lush greenery and a private garden, perfect for relaxation and unwinding.',
         location: 'Pretoria - Waterkloof',
         imageUrl: '../assets/house 8.jpg',
+        bedrooms: '6',
+        bathrooms: '3',
+        amenities: 'Swimming Pool, Parking Garage'
       },
   ];
   
