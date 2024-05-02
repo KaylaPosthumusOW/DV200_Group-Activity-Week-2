1b7aa96d477705747818c0939c170dbd5e05c0bb
Iné Smith <221076@virtualwindow.co.za>
2024-05-02 19:07:51 +0200

    nav

--- groupactivity/src/components/Navbar.jsx
+++ groupactivity/src/components/Navbar.jsx
@@ -14,6 +14,17 @@ function Navbar() {
           Home
         </Link>
 
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div class="navbar-nav">
             <Link class="nav-link-addproperty" to="/addproperty">
